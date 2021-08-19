ba0166bfd62b3f916af2ead43de68065de1c33fc
Lewis Daly <lewis@vesselstech.com>
2021-08-19 15:39:05 +0930

    feat: add a participant list service simulator

--- test/regression/pispSync.test.ts
+++ test/regression/pispSync.test.ts
@@ -6,7 +6,7 @@ const pispaSyncAPI = `http://pispa-thirdparty-scheme-adapter-outbound.${baseUrl}
 
 describe('pisp sync API', () => {
   describe('pispa', () => {
-    it('gets a list of available providers', async () => {
+    it.only('gets a list of available providers', async () => {
       // Arrange
       const uri = `${pispaSyncAPI}/linking/providers`
       console.log('GET', uri)
@@ -20,32 +20,47 @@ describe('pisp sync API', () => {
       expect(response).toStrictEqual(expected)
     })
 
-    it.only('gets a list of accounts for linking from bankone', async () => {
+    it('gets a list of accounts for linking from bankone', async () => {
       // Arrange
       const uri = `${pispaSyncAPI}/linking/accounts/bankone/61414414414`
       console.log('GET', uri)
-      const expected = {}
+      const expected = {
+        accounts: [
+          {
+            accountNickname: "Everyday Spend",
+            currency: expect.stringMatching('.*'),
+            id: expect.stringMatching('.*'),
+          }
+        ],
+        currentState: 'COMPLETED'
+      }
 
       // Act
-      const response = await axios.get(uri, {})
+      const response = (await axios.get(uri, {})).data
 
 
       // Assert
       expect(response).toStrictEqual(expected)
     })
 
+    // TODO: come back to this - waiting on a fix for the BankOne Simulator
     it.skip('returns an appropriate error when the accounts cannot be found', async () => {
       // Arrange
-      const uri = `${pispaSyncAPI}/linking/accounts/bankone/username1234`
+      const uri = `${pispaSyncAPI}/linking/accounts/bankone/blablabla`
       console.log('GET', uri)
-      const expected = {}
+      // const expected = {}
 
-      // Act
-      const response = await axios.get(uri, {})
+      // Act 
+      try {
+        await axios.get(uri, {})
+        throw new Error('Should not be executed!')
+      } catch(err) {
+        console.log(err.response)
 
 
-      // Assert
-      expect(response).toStrictEqual(expected)
+        // Assert
+        expect(err.response.status).toBe(404)
+      }
     })
   })
 })
