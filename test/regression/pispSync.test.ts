8f58ccdfb6b3456361b06b7e86943222b8ee9737
Lewis Daly <lewis@vesselstech.com>
2021-08-20 11:44:53 +0930

    feat: working on participant list sim

--- test/regression/pispSync.test.ts
+++ test/regression/pispSync.test.ts
@@ -5,8 +5,8 @@ const baseUrl = 'sandbox.mojaloop.io'
 const pispaSyncAPI = `http://pispa-thirdparty-scheme-adapter-outbound.${baseUrl}`
 
 describe('pisp sync API', () => {
-  describe('pispa', () => {
-    it.only('gets a list of available providers', async () => {
+  describe('pispa linking', () => {
+    it('gets a list of available providers', async () => {
       // Arrange
       const uri = `${pispaSyncAPI}/linking/providers`
       console.log('GET', uri)
@@ -44,7 +44,7 @@ describe('pisp sync API', () => {
     })
 
     // TODO: come back to this - waiting on a fix for the BankOne Simulator
-    it.skip('returns an appropriate error when the accounts cannot be found', async () => {
+    it.only('returns an appropriate error when the accounts cannot be found', async () => {
       // Arrange
       const uri = `${pispaSyncAPI}/linking/accounts/bankone/blablabla`
       console.log('GET', uri)
@@ -62,5 +62,22 @@ describe('pisp sync API', () => {
         expect(err.response.status).toBe(404)
       }
     })
+
+    it.todo('sends an OTP to my phone number when I start the consentRequest process')
+    it.todo('returns an error if I try to link an unknown account')
+    it.todo('returns an error if I try to link an account with a dfsp that is not bankone')
+    it.todo('returns a Consent object when I send the correct OTP')
+    it.todo('returns an error when I send an incorrect OTP')
+    it.todo('returns success when I submit a valid credential and signature')
+    it.todo('returns an error if I send a bad credential or invalid signature')
+    it.todo('allows me to lookup the consent object with the central auth-service')
+  })
+
+  describe('pispa transfer', () => {
+    it.todo('allows me to lookup a user based on a social security id alias')
+    it.todo('returns an error when I lookup a party that does not exist')
+    it.todo('when I enter in a transfer amount, it  returns an authoriztion request with details about the transfer fees')
+    it.todo('when I sign the transfer with the example payload, I get confirmation that the transfer was processed')
+    it.todo('if I send an invalid signed challenge, I get an error from the DFSP')
   })
 })
